5d11ce4d7651bc476e98178c4ddf117051959200
Achin Kulshrestha <achinkul@gmail.com>
2013-12-22 18:10:42 +0530

    Added Websocket Secure support

---
+++ socket-server2.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var options = {
+  key: fs.readFileSync('cert/dan.key'),
+  cert: fs.readFileSync('cert/dan.crt')
+};
+var app = require('https').createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+});
+var io = require('socket.io').listen(app);
+
+
+
+app.listen(8089);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+ }); 
+  
